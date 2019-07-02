12a880d0bad81e79707419df5c18d6964d777c2e
Jonghak Jung <ksgj2000@gmail.com>
2019-07-02 18:07:39 +0900

    firebase deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 const path = require('path')
+const CopyWebPackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -16,7 +17,12 @@ module.exports = {
             loaders: 'babel-loader'
         }]
     },
-    plugins: [],
+    plugins: [
+        new CopyWebPackPlugin([{
+            context: './public',
+            from: '*.*'
+        }])
+    ],
     devServer: {
         contentBase: './public',
         host: 'localhost',
